4ba0864d74d379de836ca2defe948ce02f0aa7f9
BTBMortier <ccheyap@gmail.com>
2022-02-12 06:21:00 +0100

    Created topic watcher

---
+++ topic_scraper.py
@@ -0,0 +1,41 @@
+import os 
+import sys
+import time
+import argparse
+import requests
+from pynput import keyboard
+from pynput.keyboard import KeyCode
+from bs4 import BeautifulSoup as bs
+
+
+
+def get_topics():
+    url = "https://www.jeuxvideo.com/forums/0-51-0-1-0-1-0-blabla-18-25-ans.htm"
+    page = requests.get(url)
+    soup = bs(page.content , 'html.parser')
+    body = soup.body
+    topics = body.find_all("li",class_="")
+    topics = [t for t in topics if "data-id" in t.attrs]
+    topics = [t for t in topics if t.contents[1].contents[1].attrs["class"][0] != "icon-topic-pin"]
+    return topics
+
+def get_topic_info(topics_obj):
+    display = [] 
+    for topic in topics_obj:
+            tinfos  = []
+            topax   = topic.a['title']
+            auteur  = topic.span.find_next("span",target="_blank").text.strip()
+            nposts  = topic.span.find_next("span",class_="topic-count").text.strip()
+            timest  = topic.span.find_next("span",class_="topic-date").text.strip()
+            tinfos  = [topax,auteur,nposts,timest]
+            display.append(tinfos)
+    return display
+        
+def main():
+if __name__ == '__main__': 
+main()
+
+
+
+
+ 
